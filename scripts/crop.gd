51c023e0e82adf6d9bc79ec9ae61f461e1a7b5c3
zhufree <zhufree2013@gmail.com>
2024-06-16 16:53:26 +0800

    添加新资源，新增crop和tool类

---
+++ scripts/crop.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name Crop
+
+@export var name: String
+@export var sprite: Texture
+@export var interval: int # 每个时期之间的等待时间长度
---
+++ scripts/tool.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name Tool
+
+@export var name: String
+@export var sprite: Texture
