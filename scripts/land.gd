f2a2e6f924e9ba659fd2b78e6480eb2ee1a262fd
VineMummy <guamelonlaox4@gmail.com>
2024-06-22 15:29:53 +0800

    添加单块土地的状态机

--- scripts/land.gd
+++ scripts/land.gd
@@ -1,1 +1,29 @@
 extends Control
+
+enum LandState {
+	WEED,
+	BARREN,
+	PLOWED,
+	DRY,
+	WATERED
+}
+
+var current_state = LandState.WEED
+@onready var texture_rect = $TextureRect
+
+func change_state(new_state):
+	current_state = new_state
+	update_tile()
+
+func update_tile():
+	match current_state:
+		LandState.WEED:
+			texture_rect.texture = load("res://assets/land-weed.png")
+		LandState.BARREN:
+			texture_rect.texture = load("res://assets/land-uncultivated.png")
+		LandState.PLOWED:
+			texture_rect.texture = load("res://assets/land-plowed.png")
+		LandState.DRY:
+			texture_rect.texture = load("res://assets/land-plowed.png")
+		LandState.WATERED:
+			texture_rect.texture = load("res://assets/land-watered.png")
